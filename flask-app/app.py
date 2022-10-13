6587a3c6296d6384e985d7bb9e39f5fbeefa871c
No767 <73260931+No767@users.noreply.github.com>
2022-10-13 00:24:36 -0700

    Finally figured it out...

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,7 +1,41 @@
-from flask import Flask
+import os
+from flask import Flask, session, request, redirect
+from flask_session import Session
+import spotipy
+from spotipy.cache_handler import MemoryCacheHandler, CacheFileHandler
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World! again"
+load_dotenv()
+
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
+
+
+@app.route('/')
+def index():
+
+    cache_handler = CacheFileHandler(cache_path="./.cache", username="test")
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 2. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"), as_dict=False)
+        return redirect('/')
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 1. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    # Step 3. Signed in, display data
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return f'<h2>Hi {spotify.me()["display_name"]}'
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=int(os.environ.get("PORT", os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))
